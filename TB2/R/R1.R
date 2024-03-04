66c0700fa0a869c5b6bde6a0844b16670886103e
JamesRestell8 <james.restell@icloud.com>
2024-03-04 10:53:39 +0000

    Week 19 R Lecture

---
+++ TB2/R/R1.R
@@ -0,0 +1,23 @@
+# Comments use a hashtag
+# Recommend assigning variables using <- rather than =
+
+a <- 10
+
+X <- matrix(1,10,2)
+View(X) # Inspect a matrix in a spreadsheet type window
+
+rm(a) # removes a variable from the environment
+
+typeof(X) # shows data type of variable
+
+# %% modular arithmetic
+10%%3
+
+# %/% integer division
+10%/%3
+
+# Standard division
+10/3
+
+# ^ Exponentiation
+2^10 # 2 to the 10th power
