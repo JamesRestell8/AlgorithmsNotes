61b19e802768f3608a37e625336cfeece834e6d6
JamesRestell8 <james.restell@icloud.com>
2024-03-04 10:53:58 +0000

    changes

--- TB2/R/R1.R
+++ TB2/R/R1.R
@@ -1,7 +1,7 @@
 # Comments use a hashtag
 # Recommend assigning variables using <- rather than =
 
-a <- 10
+a <- 10 # By default this is double, if you want an integer write 10L
 
 X <- matrix(1,10,2)
 View(X) # Inspect a matrix in a spreadsheet type window
@@ -21,3 +21,32 @@ typeof(X) # shows data type of variable
 
 # ^ Exponentiation
 2^10 # 2 to the 10th power
+
+vec1 <- c(1, 2, 3, 4) # This is a vector. Note that vectors are ONE-INDEXED.
+vec2 <- c(5, 6, 7, 8)
+
+vec3 <- c(vec1, vec2) # 1, 2,...,8
+
+for (i in 1:10){
+  # i takes 1 in the first iteration,
+  # i takes 2 in the second iteration ...
+  print(i)
+}
+
+a <- 1:10 # a takes all values between 1 and 10
+vec3[4:6] # gets 4th 5th 6th elements
+
+for (i in letters[1:3]){
+  cat(i) # print compactly
+}
+
+paste("a", "b")
+
+# Functions
+smaller_than_10 <- function(n){
+  if(n<10){
+    return(TRUE)
+  }else{
+    return(FALSE)
+  }
+}
